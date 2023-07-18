f62d3e93be2d179dafa3a1d8a187b4699fbe1b9c
Dasjo1995 <126097464+Dasjo1995@users.noreply.github.com>
2023-07-18 16:58:13 +0200

    Add files via upload

---
+++ Morse Code/main.py
@@ -0,0 +1,55 @@
+
+
+# Convert a sentence into morse code
+
+morse_dict = {
+    "A": ".-",
+    "B": "-...",
+    "C": "-.-.",
+    "D": "-..",
+    "E": ".",
+    "F": "..-.",
+    "G": "--.",
+    "H": "....",
+    "I": "..",
+    "J": ".---",
+    "K": "-.-",
+    "L": ".-..",
+    "M": "--",
+    "N": "-.",
+    "O": "---",
+    "P": ".--.",
+    "Q": "--.-",
+    "R": ".-.",
+    "S": "...",
+    "T": "-",
+    "U": "..-",
+    "V": "...-",
+    "W": ".--",
+    "X": "-..-",
+    "Y": "-.--",
+    "Z": "--..",
+    "1": ".----",
+    "2": "..---",
+    "3": "...--",
+    "4": "....-",
+    "5": ".....",
+    "6": "-....",
+    "7": "--...",
+    "8": "---..",
+    "9": "----.",
+    "10": "-----",
+
+}
+
+sentence = input("Which sentence would you like to translate into morse code? ")
+
+morse_sentence = ""
+
+for char in sentence:
+    if char.upper() in morse_dict:
+        morse_sentence += morse_dict[char.upper()]
+    elif char.isspace():
+        morse_sentence += " "
+
+print(f"This is your message in morse code: {morse_sentence}")
